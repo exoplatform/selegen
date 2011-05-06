21cfbb5dff6bc2ba77e6fea17c42871fa611f9c9
Linh Vu <linh.vu@exoplatform.com>
2011-05-06 04:57:44 +0000

    [SELEGEN-57] Update store commands

--- selegen-core/src/main/java/org/exoplatform/selenium/SeleniumTestCaseGenerator.java
+++ selegen-core/src/main/java/org/exoplatform/selenium/SeleniumTestCaseGenerator.java
@@ -469,7 +469,7 @@ public class SeleniumTestCaseGenerator {
 			} else if (param1.equals("storeEval")) {
 				sb.append("String ").append(param3).append(" = selenium.getEval(\"").append(param2).append("\").toString();\n");
 			} else if (param1.equals("store")) {
-				sb.append("String ").append(param3).append(" = ").append(param2).append(";\n");
+				sb.append("String ").append(param3).append(" = (\"\"").append(param2).append("\"\").toString();\n");
 			}else if (param1.equals("keyDown")) {
 				sb.append("selenium.");
 				sb.append(param1);
